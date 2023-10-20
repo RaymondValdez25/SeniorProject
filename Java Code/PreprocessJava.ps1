3f3bc854cc58c61391b25739dfa4acf70fec4f2e
Raymond Valdez <rcv5@students.uwf.edu>
2023-10-20 15:07:17 -0700

    Raymond create powershell scripts (#8)
    * committing powershell scripts
    
    * adding powershell files
    
    * committing powershell changes
    
    * removing files, I need to readd them again
    
    * Re added files
    
    * changed some data
    
    * Relative instead of absolute references

---
+++ Java Code/PreprocessJava.ps1
@@ -0,0 +1,23 @@
+# Define the file name 
+$javaFile = "ParseConnData_FollowHopsV3.java"
+
+# Compile the Java file
+javac $javaFile
+
+# Run the Java program with the "windows" parameter
+java ParseConnData_FollowHopsV3 windows
+
+#Copy FinalGraph_V4.csv from Output into Data
+cp ../Output/FinalGraph_V4.csv ../Data/
+
+$javaFile = "ParseConnData_Create_ANN_DataV3.java"
+
+#Compile the Java file
+javac $javaFile
+
+#Run java program with windows parameter
+java ParseConnData_Create_ANN_DataV3 windows
+
+#Copy the Output_NN_In_V7 and Output_OneHot_V7 into python V7 folder
+cp ../Output/Output_NN_In_V8.csv ../Code_py/V7
+cp ../Output/Output_OneHot_V8.csv ../Code_py/V7
---
+++ RunAll.ps1
@@ -0,0 +1,14 @@
+#Enter Java Code Folder
+cd 'Java Code'
+./PreprocessJava
+
+#Exit folder
+cd ..
+
+#Enter Python Code Folder
+cd 'Code_py\V7'
+
+./PreprocessPython
+
+#Exit folder
+cd ../..
