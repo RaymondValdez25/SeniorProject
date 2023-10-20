be8717bf851f6beeefe086874021ccb7f79080b6
Raymond Valdez <Raymondvaldez25@gmail.com>
2023-10-20 14:07:56 -0700

    committing powershell changes

--- Code_py/V7/PreprocessPython.ps1
+++ Code_py/V7/PreprocessPython.ps1
@@ -1,14 +1,14 @@
-# Run a Python script to convert JSON to NumPy array
+# Run a Python script to convert csv files to numpy arrays 
 python -c @"
 
 import numpy as np
 
 # Load the CSV file
 X = np.genfromtxt('Output_NN_In_V7.csv', delimiter=',')
-Y = np.genfromtxt('Output_OneHot_V7.csv', delimiter=',')
+y = np.genfromtxt('Output_OneHot_V7.csv', delimiter=',')
 
 np.save('x_V7.npy', X)
-np.save('y_V7.npy', Y)
+np.save('y_V7.npy', y)
 
 # Print the resulting NumPy array
 print(X)
--- RunAll.ps1
+++ RunAll.ps1
@@ -2,3 +2,4 @@ cd 'C:\SeniorProject\Java Code'
 ./PreprocessJava
 cd C:\SeniorProject\Code_py\V7
 ./PreprocessPython
+cd ../..
