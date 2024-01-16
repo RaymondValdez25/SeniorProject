5965b2605650d51267c06a1b5b9693c7a09aa472
Raymond Valdez <Raymondvaldez25@gmail.com>
2024-01-16 13:46:32 -0800

    made changes to python code

--- Code_py/V7/Scikit-Learn-ANN-Binary.py
+++ Code_py/V7/Scikit-Learn-ANN-Binary.py
@@ -10,9 +10,6 @@ from sklearn.metrics import confusion_matrix
 
 X = np.load('x_V8.npy') # Data
 y = np.load('y_V8.npy') # One-hot
-testDatasize = 0.10
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = testDatasize, random_state = 0, stratify=y)
 
 ann = tf.keras.models.Sequential()
 ann.add(tf.keras.layers.Dense(units=5, activation='relu'))
@@ -24,65 +21,76 @@ custom_optimizer = Adam(learning_rate=0.05)
 # Compile the model
 ann.compile(optimizer=custom_optimizer, loss='binary_crossentropy', metrics=['categorical_accuracy'])
 
-#set verbose = 2 to see accuracy line by line
-start_time_train = time.time()
-ann.fit(X_train, y_train, batch_size = 32, epochs = 100, verbose=0, validation_data=(X_test,y_test))
-end_time_train = time.time()
 
-total_training_time = f"{end_time_train - start_time_train:.2f}"
+def runANN(testDataSizePercent):
 
-#Make predictions based on X_test
-print('========...predicting========')
-start_time_test = time.time()
-y_pred = ann.predict(X_test)
-y_pred_binary = np.round(y_pred)
-end_time_test = time.time()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = testDataSizePercent, random_state = 0, stratify=y)
 
-total_testing_time = f"{end_time_test - start_time_test:.2f}"
+    #set verbose = 2 to see accuracy line by line
+    start_time_train = time.time()
+    ann.fit(X_train, y_train, batch_size = 32, epochs = 100, verbose=0, validation_data=(X_test,y_test))
+    end_time_train = time.time()
 
-print('====y_test====\n', y_test)
-print('====y_pred_binary====\n', y_pred_binary)
+    total_training_time = f"{end_time_train - start_time_train:.2f}"
 
-#Accuracy = (True Positives + True Negatives)/(True Positives + True Negatives + False Positives + False Negatives) 
-test_loss, test_accuracy = ann.evaluate(X_test, y_test)
-print('======Accuracy=====\n',test_accuracy * 100,'%')
+    #Make predictions based on X_test
+    print('========...predicting========')
+    start_time_test = time.time()
+    y_pred = ann.predict(X_test)
+    y_pred_binary = np.round(y_pred)
+    end_time_test = time.time()
 
-#Recall = (true positive)/(TruePositive + FalseNegatives)
-recall = recall_score(y_test, y_pred_binary, average='binary')
-print('======= recall ========\n', recall)
+    total_testing_time = f"{end_time_test - start_time_test:.2f}"
 
-#Precision score = (true positive)/(TruePositive + FalsePositive)
-precision = precision_score(y_test, y_pred_binary, average='binary')
-print('=======precision=======\n', precision)
+    #print('====y_test====\n', y_test)
+    #print('====y_pred_binary====\n', y_pred_binary)
 
-#F-measure False Positive Rate
-f_measure = f1_score(y_test, y_pred_binary)
-print('==========F measure======\n',f_measure)
+    print('=================================')
+    print('\nresults with test datasize:', testDataSizePercent * 100, '%')
+    print('\n=================================')
+    #Accuracy = (True Positives + True Negatives)/(True Positives + True Negatives + False Positives + False Negatives) 
+    test_loss, test_accuracy = ann.evaluate(X_test, y_test)
+    print('======Accuracy=====\n',test_accuracy * 100,'%')
 
-#AUC
-AUC = roc_auc_score(y_test, y_pred_binary)
-print('==========AUC======\n',AUC)
+    #Recall = (true positive)/(TruePositive + FalseNegatives)
+    recall = recall_score(y_test, y_pred_binary, average='binary')
+    print('======= recall ========\n', recall)
 
-#Training Time
-print('======total training time====')
-print(total_training_time, ' seconds')
+    #Precision score = (true positive)/(TruePositive + FalsePositive)
+    precision = precision_score(y_test, y_pred_binary, average='binary')
+    print('=======precision=======\n', precision)
 
-#Testing Time
-print('======total testing time====')
-print(total_testing_time, ' seconds')
+    #F-measure False Positive Rate
+    f_measure = f1_score(y_test, y_pred_binary)
+    print('==========F measure======\n',f_measure)
 
-#confusion matrix
-result = confusion_matrix(y_test, y_pred_binary).ravel()
+    #AUC
+    AUC = roc_auc_score(y_test, y_pred_binary)
+    print('==========AUC======\n',AUC)
 
-#matrix outputs
-print('\n Confusion matrix results with test datasize:', testDatasize * 100, '%')
-print('\n')
-trueNegative = result[0]
-falsePositive = result[1]
-falseNegative = result[2]
-truePositive = result[3]
+    #Training Time
+    print('======total training time====')
+    print(total_training_time, ' seconds')
 
-print('true negative:', trueNegative)
-print('false positive:', falsePositive)
-print('false negative:', falseNegative)
-print('true positive:', truePositive)
+    #Testing Time
+    print('======total testing time====')
+    print(total_testing_time, ' seconds')
+
+    #confusion matrix
+    result = confusion_matrix(y_test, y_pred_binary).ravel()
+
+    #matrix outputs
+    trueNegative = result[0]
+    falsePositive = result[1]
+    falseNegative = result[2]
+    truePositive = result[3]
+
+    print('true negative:', trueNegative)
+    print('false positive:', falsePositive)
+    print('false negative:', falseNegative)
+    print('true positive:', truePositive)
+    
+
+testDatasizePercentages = [0.10, 0.15, 0.25, 0.99]
+for testDataSizePercent in testDatasizePercentages:
+    runANN(testDataSizePercent)
